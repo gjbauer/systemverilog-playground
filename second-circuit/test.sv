e1a069b3c1134f7749f847b7b03c77e9810c9e4f
Gabriel Bauer <gabeb1277@gmail.com>
2026-03-27 16:49:31 -0400

    first testbench

--- second-circuit/test.sv
+++ second-circuit/test.sv
@@ -5,7 +5,6 @@ module adder(
 	output logic Cout
 );
 
-	wire w1, w2, w3;
 	logic Cin[7:0];
 
 	assign Cin[0] = 1'b0;
@@ -13,14 +12,11 @@ module adder(
 	genvar i;
 	generate
 		for (i=0; i < 8; i = i + 1) begin
-			assign w1 = A[i] ^ B[i];
-			assign Sum[i] = w1 ^ Cin[i];
-			assign w2 = w1 & Cin[i];
-			assign w3 = A[i] & B[i];
+			assign Sum[i] = (A[i] ^ B[i]) ^ Cin[i];
 			if ( i < 7 ) begin
-				assign Cin[i+1] = w2 | w3;
+				assign Cin[i+1] = (A[i] ^ B[i]) & Cin[i] | (A[i] & B[i]);
 			end else begin
-				assign Cout = w2 | w3;
+				assign Cout = (A[i] ^ B[i]) & Cin[i] | (A[i] & B[i]);
 			end
 		end
 	endgenerate
---
+++ second-circuit/testbench.sv
@@ -0,0 +1,38 @@
+module tb_adder;
+
+	logic [7:0] tb_a;
+	logic [7:0] tb_b;
+	logic [7:0] tb_sum;
+	logic tb_cout;
+
+	adder dut_inst (
+		.A(tb_a),
+		.B(tb_b),
+		.Sum(tb_sum),
+		.Cout(tb_cout)
+	);
+
+	initial begin
+		tb_a = 8'd1;
+		tb_b = 8'd1;
+		#10;
+		check_output(8'd2, 0);
+
+		tb_a = 8'd255;
+		tb_b = 8'd1;
+		#10;
+		check_output(8'd0, 1);
+
+	end
+
+	task check_output(logic [7:0] expected_sum, logic expected_cout);
+		if (tb_sum === expected_sum && tb_cout === expected_cout) begin
+			$display("PASS!!");
+			$display("Sum = %0d, carry = %0d", tb_sum, tb_cout);
+		end else begin
+			$display("FAIL!!");
+			$display("Sum = %0d, expected %0d", tb_sum, expected_sum);
+			$display("Carry = %0d, expected %0d", tb_cout, expected_cout);
+		end
+	endtask
+endmodule
